f9eef1038db6e922c3f6fff517d5a714f690e0de
Oliver Bayes-Shelton <bayesshelton.oliver@gmail.com>
2012-08-28 20:08:44 +0100

    Bens changes

---
+++ core.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {  
+    $('.port_btn').click(function () {
+           show();
+        });
+    $('.btn_close').click(function () {
+           hide();
+        });
+});
+
