9da85850914ca74e93acb1887f0134f5d54c5b72
Ubuntu <ubuntu@ip-10-235-82-128.eu-west-1.compute.internal>
2012-10-03 18:09:32 +0000

    Tracking code

--- index.php
+++ index.php
@@ -6,6 +6,22 @@
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.js"></script>
 		<script type="text/javascript" src="core.js"></script>
 		<script type="text/javascript" src="plugins.js"></script>
+
+
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-27998086-4']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
 	</head>
 	<body class="clearfix">
 		<div id="head_area">
